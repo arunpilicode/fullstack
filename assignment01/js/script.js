3a4067c4fa5cc43d51d585778e0df1fb01ec0457
arunpilicode <arunpilicode@gmail.com>
2018-11-11 12:26:33 +0530

    first assignment

---
+++ assignment01/js/script.js
@@ -0,0 +1,20 @@
+function read(){
+	var name = document.getElementById("name").value;
+	var address = document.getElementById("address").value;
+	var gender = document.getElementById("gender").value;
+	var age = document.getElementById("age").value;
+	var dob = document.getElementById("dob").value;
+	var dob = document.getElementById("mob").value;
+	var dob = document.getElementById("email").value;
+	var district = document.getElementById("district");
+	var dist_select = district.options[district.selectedIndex].value;
+	console.log(name);
+	console.log(address);
+	console.log(gender);
+	console.log(age);
+	console.log(dob);
+	console.log(district_select);
+	console.log(mob);
+	console.log(email);
+	
+}	
